6bf9da42b215774eea9bf3d18bd39737d86dc74b
semper_idem <semper_idem@126.com>
2015-08-31 11:59:22 +0800

    Init Submit
    Init Submit

---
+++ CrazyBounce-OC/Controller/GameBeginViewController.h
@@ -0,0 +1,17 @@
+//
+//  GameBeginViewController.h
+//  CrazyBounce-OC
+//
+//  Created by 星夜暮晨 on 2015-05-04.
+//  Copyright (c) 2015 益行人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "waterView.h"
+
+@interface GameBeginViewController : UIViewController <waterViewDelegate>
+
+@property waterView* waterWaveView;
+@property CGFloat waveHeight;
+
+@end
